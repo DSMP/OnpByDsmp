9088c0ace126c7b687fe72a18c9b6c0e4c255359
damian <damiakuz@wp.pl>
2018-06-04 00:10:14 +0200

    initial finished program

---
+++ ONPWithStack/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ONPWithStack
+{
+    class Program
+    {
+        static List<char> _output;
+        static void Main(string[] args)
+        {
+            _output = new List<char>();
+            Console.Write("Podaje poprawne wyrażenie: ");
+            //string input = Console.ReadLine();
+            string input = "((2*5+1)/2)=";
+            if (input[input.Length-1] != '=')
+            {
+                input += '=';
+            }
+            NormalToONP(input);
+            Console.Write("Output: ");
+            foreach (var item in _output)
+            {
+                Console.Write(item + " ");
+            }
+
+        }
+        static public void NormalToONP(string normalArg)
+        {
+            Stack OnpStack = new Stack();
+            foreach (char item in normalArg.ToCharArray())
+            {
+                if (item == '(' || _isOperator(item))
+                {
+                    char onStack = OnpStack.Pop();
+                    if (onStack != '\0')
+                    {
+                        if ((item == '*' || item == '/') && (onStack == '+' || onStack == '-') ||
+                            (item == '^') && (onStack == '+' || onStack == '-' || onStack == '*' || onStack == '/'))
+                        {
+                            _output.Add(item);
+                            continue;
+                        }
+                        OnpStack.Push(onStack);
+                        if ((item == '+' || item == '-') && (onStack == '*' || onStack == '/'))
+                        {
+                            _output.Add(OnpStack.Pop());
+                            OnpStack.Push(item);
+                            continue;
+                        }
+                    }
+                    OnpStack.Push(item);
+                    continue;
+                }
+                if (_isNumeric(item))
+                {
+                    _output.Add(item);
+                }
+                if (item == ')')
+                {
+                    char c;
+                    while ((c = OnpStack.Pop()) != '(')
+                    {
+                        _output.Add(c);
+                    }
+                }
+                if (item == '=')
+                {
+                    while (OnpStack.Length > 0)
+                    {
+                        _output.Add(OnpStack.Pop());
+                    }
+                }
+            }
+        }
+
+        private static bool _isOperator(char item)
+        {
+            if (item == '+' || item == '-' || item == '*' || item == '/')
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private static bool _isNumeric(char item)
+        {
+            try
+            {
+                var numnber = Convert.ToInt32(item);
+                if (item == '=' || item == ')')
+                {
+                    return false;
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return true;
+        }
+
+    }
+}
---
+++ ONPWithStack/Stack.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ONPWithStack
+{
+    public class Stack
+    {
+        public int Length { get; set; }
+        List<char> _stack;
+        int _index;
+        public Stack()
+        {
+            _index = 0;
+            _stack = new List<char>();
+            Length = 0;
+        }
+        public void Push(char element)
+        {
+            lock (this)
+            {
+                _index++;
+                Length++;
+                _stack.Add(element);
+            }
+        }
+        public char Pop()
+        {
+            lock(this)
+            {
+                try
+                {
+                    Length--;
+                    char c = _stack[--_index];
+                    _stack.RemoveAt(_index);
+                    return c;
+                }
+                catch (ArgumentOutOfRangeException)
+                {
+                    Length = 0;
+                    _index = 0;
+                    return '\0';
+                }
+            }
+        }
+
+    }
+}
